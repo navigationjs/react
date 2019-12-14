656ee42154922fe25071931beec3eb3e9a6c8292
Ivan Zotov <ivan_zotov@railsmob.com>
2019-12-14 20:18:55 +0300

    Add Modal

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import navigation, {
   toId,
   fromId,
 } from '@navigationjs/core';
+import Modal from './src/Modal';
 
-export { Navigation, Base, Value, toId, fromId };
+export { Navigation, Base, Modal, Value, toId, fromId };
 export default navigation;
