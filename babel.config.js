1f198e6a6e1b1e8968b2923ff5c4a7d82a60270e
Ivan Zotov <ivan_zotov@railsmob.com>
2019-12-14 22:00:42 +0300

    Fix babel presets

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,4 @@
 module.exports = {
-  presets: [
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current',
-        },
-      },
-    ],
-    '@babel/preset-react'
-  ],
-  plugins: ['@babel/plugin-proposal-class-properties']
-}
+  presets: ['@babel/preset-react'],
+  plugins: ['@babel/plugin-proposal-class-properties'],
+};
