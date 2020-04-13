72a7461931b5fc1b30a5c8991505a31406031d66
Ivan Zotov <ivan_zotov@epam.com>
2020-04-13 11:45:13 +0300

    Update core and events. Bump to 0.7.0

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  presets: ['@babel/preset-react'],
+  presets: ['@babel/preset-react', ['@babel/preset-env', {
+    targets: {
+      browsers: ["last 2 versions", "ie >= 9"] 
+    }
+  }]],
   plugins: [
     '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-  ],
+    '@babel/plugin-proposal-object-rest-spread'
+  ]
 };
